351488c9a9fba35f8a0d851e758a20df14ed73ed
Blanca <blancabcbt@gmail.com>
2022-07-15 16:50:53 +0200

    Add event listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//QUERY SELECTOR
+const input = document.querySelector('.js-input');
+const btnSearch = document.querySelector('.js-btn-search');
+const btnReset = document.querySelector('.js-btn-reset');
+
+
+function handleClick(ev) {
+    ev.preventDefault();
+    let inputValue = input.value;
+    console.log(inputValue);
+
+}
+
+btnSearch.addEventListener('click', handleClick);
